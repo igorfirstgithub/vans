035f4dcfda3b82af775002bb8a6c09cf65724769
Igor <bigbookreader@gmail.com>
2023-07-20 11:32:13 +0300

    Starting point of VanLife project

--- src/index.js
+++ src/index.js
@@ -1,4 +1,46 @@
-import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import "./index.css";
+
+import "./server";
+
+/**
+ * Challenge: Create the Vans list page, Route, and Link
+ *
+ * 1. Create a Vans component in the "pages" directory. For now,
+ *    just render <h1>Vans page goes here 🚐</h1>
+ * 2. Create a Route for the Vans page on the /vans route
+ * 3. Add a Link in the nav bar to the Vans route
+ */
+
+function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">
+          #VanLife
+        </Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+//***** Initial page */
+
+/**
+ * import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -15,3 +57,7 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+ * 
+ * 
+ */
